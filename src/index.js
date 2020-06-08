b3303822a75aa94ffada136892038336cd08a5ac
MrAhsanKhan <66264891+MrAhsanKhan@users.noreply.github.com>
2020-06-08 21:52:55 +0500

    create basic react app

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div><App name="Mr.Ahsan Khan" age={34} /></div>,
   document.getElementById('root')
 );
 
